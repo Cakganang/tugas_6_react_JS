af027cf7b9dae237c17454a518c62ff5e26913b2
sonny faristrianto <sonnyf4r15@gmail.com>
2024-06-15 17:55:44 +0700

    tambahkan file file

---
+++ Lib/DaftarMakanan.js
@@ -0,0 +1,29 @@
+const DaftarMakanan =         [
+    {
+        id: 1,
+        NamaMakanan: "Nasi Padang",
+        harga: 20000
+    },
+    {
+        id: 2,
+        NamaMakanan: "Sate",
+        harga: 20000
+    },
+    {
+        id: 3,
+        NamaMakanan: "Soto",
+        harga: 10000
+    },
+    {
+        id: 4,
+        NamaMakanan: "Uduk",
+        harga: 10000
+    },
+    {
+        id: 5,
+        NamaMakanan: "Nasi Kuning",
+        harga: 15000
+    }
+];
+
+export default DaftarMakanan;
---
+++ Lib/DaftarMinuman.js
@@ -0,0 +1,24 @@
+const DaftarMinuman =         [
+    {
+        nama: "Es Cendol",
+        harga: 10000
+    },
+    {
+        nama: "Es Pisang Hijau",
+        harga: 10000
+    },
+    {
+        nama: "Es Kacang Merah",
+        harga: 10000
+    },
+    {
+        nama: "Es Pleret",
+        harga: 10000
+    },
+    {
+        nama: "Es Legen",
+        harga: 15000
+    }
+];
+
+export default DaftarMinuman;
